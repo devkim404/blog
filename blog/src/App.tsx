16edcb03951cdddbfe47222da0cfb86eb4efe249
devkim404 <120579845+devkim404@users.noreply.github.com>
2023-02-19 01:54:14 +0900

    개발환경 구축

---
+++ blog/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+import Exhibition from './blog/components/Exhibition/Exhibition';
+
+function App() {
+  return (
+    <Exhibition />
+  );
+}
+
+export default App;
---
+++ blog/src/blog/components/Exhibition/Exhibition.tsx
@@ -0,0 +1,35 @@
+import { Container, CardGroup, Card, Button } from 'react-bootstrap';
+
+export default function Exhibition() {
+  return (
+    <>
+      <Container className="exhibition">
+        <CardGroup>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Container>
+    </>
+  )
+}
